6c41ed7475f1454f713e767bd35c26babc835ed4
Fethi Çetin <fthi.cetin@gmail.com>
2021-04-28 22:21:33 +0300

    Update main.py

--- CASE2/CaseTwo/main.py
+++ CASE2/CaseTwo/main.py
@@ -1,8 +1,11 @@
 import json
 import flask
 import requests
+from flask import jsonify
 from requests.auth import HTTPBasicAuth
 from InternalSettings import InternalSettings
+from couchbase.cluster import Cluster
+from couchbase.auth import PasswordAuthenticator
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -14,9 +17,23 @@ BASE_URL = "http://localhost:8075"
 def getInternalSettings():
     url = BASE_URL + "/internalSettings"
     response = requests.get(url=url, auth=HTTPBasicAuth('Administrator', '123456'))
-    settingsDictionary = json.loads(json.dumps(response.json()))
-    internalSettings = InternalSettings(**settingsDictionary)
+    jsonString = json.loads(json.dumps(response.json()))
+    internalSettings = InternalSettings(**jsonString)
     return json.dumps(internalSettings._asdict())
+
+
+@app.route('/getEmployee', methods=['GET'])
+def getEmployee():
+    cluster = Cluster('couchbase://172.17.0.4', authenticator=PasswordAuthenticator('Administrator', '123456'))
+    employee = cluster.bucket('employee')
+    coll = employee.default_collection()
+    employeeDoc = {
+        "name": "Eda",
+        "surname": "Şahinli"
+    }
+    coll.upsert("employee1", employeeDoc)
+    result = coll.get("employee1")
+    return jsonify(result.content_as[str])
 
 
 app.run()
